3bdc74f3669667ef51db272a3427bf52e528d841
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-09-30 13:12:46 +0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import './App.css';
-import { HW1 } from './HW/1/HW1';
-import { HW2 } from './HW/2/HW2';
-import { HW3 } from './HW/3/HW3';
-import { HW4 } from './HW/4/HW4';
+import OnOff from "./Components/onOff/OnOff";
+
 
 function App() {
   return (
-    <div className="App">
-        {/*<HW1 />>*/}
-        <HW2 />
-      {/*<HW3 />*/}
-      {/*<HW4 />*/}
+    <div >
+      <OnOff on={false}/>
+      {/*<OnOff on={true}/>*/}
+      {/*<OnOff on={false}/>*/}
     </div>
   );
 }
